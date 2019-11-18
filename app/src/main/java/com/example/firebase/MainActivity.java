c3d0db33fea8dd7604483a5800ff21f4206471e1
kyjin2881 <kyjin2881@ajou.ac.kr>
2019-11-18 19:34:22 +0900

    commit

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // test1
+        // test1121
 
         // 현재 로그인 되어있는지 확인 로그인 되어있지 않으면 로그인 화면으로 넘어가도록
         if(FirebaseAuth.getInstance().getCurrentUser() == null){
